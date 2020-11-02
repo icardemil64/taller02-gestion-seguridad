dbb747f2d494fadd4b58d09c88071011afac7ccd
icardemil64 <38365813+icardemil64@users.noreply.github.com>
2020-11-02 19:16:00 -0300

    Correción de cifrado

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const moduloEncriptador = (() => {
    
-    const alfBase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    const alfBase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'];
+    const largoAlf = alfBase.length;
     let textoCifrado, textoClaro, secuencia,k1,k2;
     const btnDescifrado = document.getElementById("btnDescifrado");
     const btnCifrado = document.getElementById("btnCifrado");
@@ -14,15 +15,38 @@ const moduloEncriptador = (() => {
     }
 
     const obtenerEntradas = () => {
-        k1 = document.getElementById('inputClave1').value,
-        k2 = document.getElementById('inputClave2').value,
-        secuencia = document.getElementById('inputSecuencia').value,
-        textoCifrado = document.getElementById('inputTextoCifrado');
-        textoClaro = document.getElementById('inputTextoClaro');
+        k1 = document.getElementById('inputClave1').value;       
+        k2 = document.getElementById('inputClave2').value;
+        secuencia = document.getElementById('inputSecuencia').value; 
+        if( k1 == "" || secuencia == ""|| k2 == ""){
+            alert("Rellene los campos K1, K2 y secuencia");
+            return false;
+        }
+        else{
+            if( (k1>=1 && k1<=26) && (k2>=1 && k2<=26) && (secuencia.length>=2 && secuencia.length<=6)){
+                textoCifrado = document.getElementById('inputTextoCifrado');
+                textoClaro = document.getElementById('inputTextoClaro');
+                return true;
+            }
+            else{
+                    alert("K1 y K2 deben ser un numero entre 1 y 26 \n y secuencia debe tener un largo entre 2 y 6" )
+                }
+        }
+        
     }
 
-    const descifrarTexto = () => {
-        
+    const descifrarTexto = (str) => {
+        let textoTempDes = str.toLowerCase()
+        let textoResult = ""
+        let secIndexDes = 0;
+        for(let i=0; i < textoTempDes.length; i++) {
+            textoResult += encontrarCaracter(textoTempDes[i],secuencia[secIndexDes] === '1'? k1:k2, false)
+            secIndexDes++;
+            if(secIndexDes=== secuencia.length){
+                secIndexDes = 0;
+            }
+        }
+        return textoResult.split("x").join(" ").toUpperCase()
     }
 
     const cifrarTexto = (str) => {
@@ -30,7 +54,7 @@ const moduloEncriptador = (() => {
         let textoRes = ""
         let secIndex = 0;
         for(let i = 0; i < textoTemp.length; i++){
-            textoRes += encontrarCaracter(textoTemp[i],secuencia[secIndex] === '1'? k1:k2);
+            textoRes += encontrarCaracter(textoTemp[i],secuencia[secIndex] === '1'? k1:k2, true);
             secIndex++;
             if(secIndex === secuencia.length){
                 secIndex = 0;
@@ -39,26 +63,56 @@ const moduloEncriptador = (() => {
         return textoRes.toUpperCase()
     }
 
-    const encontrarCaracter = (letra,clave) => {
-        let letraIndex = parseInt(alfBase.indexOf(letra)) + parseInt(clave);
-        let largoAlf = alfBase.length;
-        if(letraIndex >= largoAlf){
-            letraIndex = (largoAlf - letraIndex)*(-1)
+    const encontrarCaracter = (letra,clave, accion) => {
+        letraIndex=0;
+        if(accion){
+            letraIndex = parseInt(alfBase.indexOf(letra)) + parseInt(clave);
+            if(letraIndex >= largoAlf){
+                letraIndex  = (largoAlf - letraIndex)*(-1)
+            }
         }
-        console.log(letraIndex)
+        else{
+
+            letraIndex = parseInt(alfBase.indexOf(letra)) - parseInt(clave)
+            if(letraIndex < 0){
+                letraIndex = (largoAlf + letraIndex)
+            }
+        }
         return(alfBase[letraIndex]);
     }
 
     btnDescifrado.addEventListener('click', () => {
-        obtenerEntradas();
+        if(obtenerEntradas()){
+            if(textoCifrado.value != ""){
+                textoClaro.value = descifrarTexto(textoCifrado.value);
+            }
+            else{
+                alert("Ingrese el texto cifrado a Descifrar");
+            }
+        }
+        
     })
 
-    btnCifrado.addEventListener('click', () => {
-        obtenerEntradas();
-        textoCifrado.value = cifrarTexto(textoClaro.value);
+    btnCifrado.addEventListener('click', () => { 
+        if(obtenerEntradas()){        
+            if(textoClaro.value != ""){
+                textoCifrado.value = cifrarTexto(textoClaro.value);
+            }
+            else{
+                alert("Ingrese el texto claro a cifrar");
+            }
+        }      
     })
 
     return{
         nuevoEncriptador : limpiarEncriptador,
     }
 })();
+/*
+
+ORSDFOCPLR
+k1 8 k2 3
+c1c2c1c2
+
+
+*/
